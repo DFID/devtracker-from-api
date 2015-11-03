7dcc1e274c1159a7c14859d246b32b66707f3298
Gazi <g-shafiullah@dfid.gov.uk>
2015-11-03 16:12:57 +0600

    OIPA Link change

--- devtracker.rb
+++ devtracker.rb
@@ -30,10 +30,10 @@ include CommonHelpers
 include ResultsHelper
 
 # Developer Machine: set global settings
-set :oipa_api_url, 'http://dfid-oipa.zz-clients.net/api/'
+#set :oipa_api_url, 'http://dfid-oipa.zz-clients.net/api/'
 
 # Server Machine: set global settings
-#set :oipa_api_url, 'http://127.0.0.1:6081/api/'
+set :oipa_api_url, 'http://127.0.0.1:6081/api/'
 
 #ensures that we can use the extension html.erb rather than just .erb
 Tilt.register Tilt::ERBTemplate, 'html.erb'
