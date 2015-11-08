2a7520ecf003ef6041551e7b3485b134020dc53b
Auninda <a-saleque@dfid.gov.uk>
2015-11-08 10:34:57 +0600

    updated the code to avoid clash with null values.

--- public/javascripts/searchPageFilters.js
+++ public/javascripts/searchPageFilters.js
@@ -174,7 +174,7 @@ $(document).ready(function() {
                         validResults['total_child_budget_value'] = !isEmpty(result.activity_aggregations.total_child_budget_value) ? result.activity_aggregations.total_child_budget_value : 0;
                         validResults['activity_status'] = !isEmpty(result.activity_status.name) ? result.activity_status.name : "";
                         validResults['reporting_organisations'] = !isEmpty(result.reporting_organisations[0].narratives[0]) ? result.reporting_organisations[0].narratives[0].text : "";
-                        validResults['description'] = !isEmpty(result.description[0]) ? result.description[0].narratives[0].text : "";
+                        validResults['description'] = !isEmpty(result.description[0].narratives[0]) ? result.description[0].narratives[0].text : "";
                         var tempString = '<div class="search-result"><h3><a href="/projects/'+validResults['iati_identifier']+'">'+validResults['title']+' <small>['+ validResults['iati_identifier'] +']</small></a></h3><span class="budget">Budget: <em> £'+addCommas(validResults['total_child_budget_value'])+'</em></span><span>Status: <em>'+validResults['activity_status']+'</em></span><span>Reporting Org: <em>'+validResults['reporting_organisations']+'</em></span><p class="description">'+validResults['description']+'</p></div>';
                         $('#showResults').append(tempString);
                     });
@@ -212,7 +212,7 @@ $(document).ready(function() {
                 validResults['total_child_budget_value'] = !isEmpty(result.activity_aggregations.total_child_budget_value) ? result.activity_aggregations.total_child_budget_value : 0;
                 validResults['activity_status'] = !isEmpty(result.activity_status.name) ? result.activity_status.name : "";
                 validResults['reporting_organisations'] = !isEmpty(result.reporting_organisations[0].narratives[0]) ? result.reporting_organisations[0].narratives[0].text : "";
-                validResults['description'] = !isEmpty(result.description[0]) ? result.description[0].narratives[0].text : "";
+                validResults['description'] = !isEmpty(result.description[0].narratives[0]) ? result.description[0].narratives[0].text : "";
                 var tempString = '<div class="search-result"><h3><a href="/projects/'+validResults['iati_identifier']+'">'+validResults['title']+' <small>['+ validResults['iati_identifier'] +']</small></a></h3><span class="budget">Budget: <em> £'+addCommas(validResults['total_child_budget_value'])+'</em></span><span>Status: <em>'+validResults['activity_status']+'</em></span><span>Reporting Org: <em>'+validResults['reporting_organisations']+'</em></span><p class="description">'+validResults['description']+'</p></div>';
                 $('#showResults').append(tempString);
             });
