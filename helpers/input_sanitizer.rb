8ee040c6645a9717ee0b0a1473a558a329791b2f
johnadamsDFID <j-adams@dfid.gov.uk>
2015-11-12 10:46:39 +0000

    Added spaces into input sanitizer

--- helpers/input_sanitizer.rb
+++ helpers/input_sanitizer.rb
@@ -3,13 +3,13 @@ module InputSanitizer
 		case type
 		when "e"
 			#This one sanitizes an email input
-			return string.gsub(/[^a-z0-9+_.@- ]/,'')
+			return string.gsub(/[^a-z0-9+_.@-\s]/,'')
 		when "a"
 			#Handles the free text search inputs
-			return string.gsub(/[^a-zA-Z0-9_\/\- ]/,'')
+			return string.gsub(/[^a-zA-Z0-9_\/\-\s]/,'')
 		else
 			#anything other than those specified will act similar to the case 'a' by default
-			return string.gsub(/[^a-zA-Z0-9_\/\- ]/,'')
+			return string.gsub(/[^a-zA-Z0-9_\/\-\s]/,'')
 		end
 	end
 end
