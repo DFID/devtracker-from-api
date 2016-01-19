5d04419207335222b868efe3f871b596d3e5ece2
Gazi <g-shafiullah@dfid.gov.uk>
2016-01-19 14:01:09 +0600

    1. Add new Results Link for MYR 2015 Report in Index Page. 2. Change two Results value as instructed.

--- devtracker.rb
+++ devtracker.rb
@@ -53,7 +53,7 @@ include RecaptchaHelper
 #set :oipa_api_url, 'http://loadbalancer1-dfid.oipa.nl/api/'
 
 # Server Machine: set global settings to use varnish cache
-set :oipa_api_url, 'http://127.0.0.1:6081/api/'
+#set :oipa_api_url, 'http://127.0.0.1:6081/api/'
 
 #ensures that we can use the extension html.erb rather than just .erb
 Tilt.register Tilt::ERBTemplate, 'html.erb'
