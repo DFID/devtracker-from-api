5ece25abb0d258ceb419f3ebcac9b1d28cf4d61a
Auninda <a-saleque@dfid.gov.uk>
2016-12-21 13:14:03 +0600

    code updated.

--- devtracker.rb
+++ devtracker.rb
@@ -54,11 +54,11 @@ include OGDHelper
 # Developer Machine: set global settings
 #set :oipa_api_url, 'https://devtracker.dfid.gov.uk/api/'
 #set :oipa_api_url, 'http://loadbalancer1-dfid.oipa.nl/api/'
-#set :oipa_api_url, 'http://staging-dfid.oipa.nl/api/'
+set :oipa_api_url, 'http://staging-dfid.oipa.nl/api/'
 #set :oipa_api_url, 'https://dev-dfid.oipa.nl/api/'
 
 # Server Machine: set global settings to use varnish cache
-set :oipa_api_url, 'http://127.0.0.1:6081/api/'
+#set :oipa_api_url, 'http://127.0.0.1:6081/api/'
 
 #ensures that we can use the extension html.erb rather than just .erb
 Tilt.register Tilt::ERBTemplate, 'html.erb'
