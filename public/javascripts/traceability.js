e23989956f7e60b82b8c94434f9040c9c25b04fc
Open Source Development <osd@Opens-Air.home>
2017-05-24 11:09:53 +0100

    begin on traceability toolbar

--- public/javascripts/traceability.js
+++ public/javascripts/traceability.js
@@ -1,59 +1,33 @@
 "use strict";
 
 
-
 jQuery( document ).ready(function($) {
 
-
 // OIPA URL - TODO - make dynamic
-
 var oipaApiUrl = "https://dc-dfid.oipa.nl/api/";
 
-
-
 // variables for the visualisation
-
 var svg;
-
 var margin = { top: 0, right: 0, bottom: 0, left: 0 },
-
     width = 600 - margin.right - margin.left,
-
     height = 20000 - margin.top - margin.bottom,
-
     nodeWidth = 20,
-
     nodeHeight = 20;
-
-
 
 var colors = [d3.rgb(0, 130, 112), d3.rgb(58, 186, 168), d3.rgb(233, 186, 130), d3.rgb(59, 101, 126), d3.rgb(177, 68, 76), d3.rgb(0, 94, 165), d3.rgb(48, 48, 48), d3.rgb(48, 48, 48), d3.rgb(48, 48, 48), d3.rgb(48, 48, 48), d3.rgb(48, 48, 48), d3.rgb(48, 48, 48)];
 
-
-
 var first = 1;
 
-
-
 // this defined the height difference per tier
-
 var y = d3.scale.ordinal().domain(d3.range(10)).rangePoints([0, 150 * 10], 1);
 
 
-
 var Chain = function Chain(activityId) {
-
   this._activityId = activityId;
 
-
-
   this.chainId = null;
-
   this.links = null;
-
   this.activities = null;
-
-
 
   this.errors = null;
 
@@ -68,12 +42,9 @@ var Chain = function Chain(activityId) {
 };
 
 
-
 Chain.prototype.getChain = function () {
 
   var that = this;
-
-
 
 //  $.getJSON(oipaApiUrl + "chains/?format=json&includes_activity=" + this._activityId, function (data) {
     $.getJSON("/getChains?project=" + this._activityId, function (data) {
@@ -82,156 +53,81 @@ Chain.prototype.getChain = function () {
     console.log(data.output);
     that.chainId = data.output.results[0].id;
 
-
-
     that.getLinks();
-
     that.getActivities();
-
   });
-
 };
-
 
 
 Chain.prototype.getLinks = function () {
-
   var that = this;
-
-
-
   //$.getJSON(oipaApiUrl + "chains/" + that.chainId + "/links/?format=json", function (data) {
   $.getJSON("/getLinks?chainId="+that.chainId, function (data) {
     // console.log(data)
-
     that.links = data.output;
-
     that.loadChainWhenReady();
-
   });
-
 };
 
-
-
 Chain.prototype.getActivities = function () {
-
   var that = this;
-
-
  
   //$.getJSON(oipaApiUrl + "chains/" + that.chainId + "/activities/?format=json&page_size=400&fields=iati_identifier,title,reporting_organisation", function (data) {
     $.getJSON("/getActivities?chainId="+that.chainId, function (data) {
 
-
     var activities = {};
 
-
-
     data.output.results.forEach(function (activity) {
-
       activities[activity.iati_identifier] = activity;
-
     });
 
-
-
     that.activities = activities;
-
     that.loadChainWhenReady();
-
   });
-
 };
-
-
 
 Chain.prototype.loadChainWhenReady = function () {
-
   if (this.activities != null && this.links != null & !this.chainCreated) {
-
     this.createChain();
-
     this.chainCreated = true;
-
   }
-
 };
-
 
 
 // from here on down, its pretty much d3 scripting
-
 Chain.prototype.createChain = function () {
-
   // console.log('time to create the chain')
-
-
-
   // SET THE DATA UP
-
-
-
   // create d3 visualisation
-
-
-
   // the visualisation is based upon https://stackoverflow.com/questions/21598092/d3-force-layout-with-fixed-y-ranges-reducing-link-overlap
-
-
+    
 
   var activities = this.activities;
-
-
-
   var nodesObj = {};
 
-
-
   // for each link, check if the start node and end node are in nodesObj. If not add them.
-
-  this.links.forEach(function (link) {
-
-
+  
+    var links = this.links
+    links.forEach(function (link) {
 
     if (!nodesObj[link.start_node.activity_iati_id]) {
-
-
-
       nodesObj[link.start_node.activity_iati_id] = {
-
         _id: link.start_node.activity_iati_id,
-
         level: link.start_node.tier,
-
         bol: link.start_node.bol,
-
         eol: link.start_node.eol,
-
-        activity: activities[link.start_node.activity_oipa_id]
-
+        activity: activities[link.start_node.activity_iati_id]
       };
-
     }
 
-
-
     if (!nodesObj[link.end_node.activity_iati_id]) {
-
-
-
+        
       nodesObj[link.end_node.activity_iati_id] = {
-
         _id: link.end_node.activity_iati_id,
-
         level: link.end_node.tier,
-
         bol: link.end_node.bol,
-
         eol: link.end_node.eol,
-
-        activity: activities[link.end_node.activity_oipa_id]
-
+        activity: activities[link.end_node.activity_iati_id]
       };
 
     }
@@ -361,31 +257,17 @@ var root = svg.select('#spe');
 
   });
 
-
-
   legenNode.append("text").attr("x", 42).attr("y", function (d) {
-
     return d * 24 + 32;
-
   }).attr("fill", "black").text(function (d) {
-
     return "tier " + d;
-
   });
 
 
-
   // create the links
-
-  var link = svg.selectAll(".link").data(graph.links).enter().append("line").attr("class", "link").style("stroke-width", 2).style("stroke", "#999");
-
-  // .on("click", clickLink())
-
-  // .on("mouseenter", mouseOverLink())
-
-  // .on("mouseout", mouseOutLink);
-
-
+  var link = svg.selectAll(".link").data(graph.links).enter().append("line").attr("class", "link").style("stroke-width", 2).style("stroke", "#999")
+   .on("mouseenter", mouseOverLink)
+   .on("mouseout", mouseOutLink);
 
   // create the nodes
 
@@ -405,11 +287,10 @@ var root = svg.select('#spe');
 
     return colors[d.level].darker(2);
 
-  });
+  })
+    .on("mouseenter", mouseOverNode);
 
-  // .on("click", clickNode())
 
-  // .on("mouseenter", mouseOverNode());
 
 
 
@@ -428,158 +309,54 @@ var root = svg.select('#spe');
   setTimeout(function () {
 
     first = 0;
-
     force.start();
-
   }, 1000);
-
-
 
   function tick(e) {
 
-
-
     var k = first ? e.alpha : 5 * e.alpha;
 
-
-
     graph.nodes.forEach(function (o, i) {
-
       o.y += (y(o.level) - o.y) * k;
-
     });
-
-
 
     node.attr("x", function (d) {
-
       return d.x;
-
     }).attr("y", function (d) {
-
       return d.y;
-
     });
-
-
 
     link.attr("x1", function (d) {
-
       return d.source.x + nodeWidth / 2;
-
     }).attr("y1", function (d) {
-
       return d.source.y + nodeHeight / 2;
-
     }).attr("x2", function (d) {
-
       return d.target.x + nodeWidth / 2;
-
     }).attr("y2", function (d) {
-
       return d.target.y + nodeHeight / 2;
-
     });
-
   }
 
-
-
   // when loading, show th graph with limited visibility in the background
-
   svg.style("opacity", 0.01).transition().duration(1000).style("opacity", 0.02).transition().duration(1000).style("opacity", 1);
-
-  // function clickNode(d){
-
-  //   var self = this;
-
-  //   return function(d, i) {
-
-  //       const nodeLocked = self.state.node.index === d.index ? !self.state.nodeLocked : self.state.nodeLocked
-
-  //       self.setState({
-
-  //         node: d,
-
-  //         nodeLocked:nodeLocked
-
-  //       })
-
-  //   }
-
-  // }
+  
 
 
+   function mouseOverNode(d) {
+        // display info on the node in the sidebar
+        $("#traceability-node-info").html("<strong>title</strong><br>" + d.activity.title.narratives[0].text + "<br><strong>reporting-org</strong><br>" + d.activity.reporting_organisation.narratives[0].text)
+   }
 
-  // function clickLink(d){
-
-  // var self = this;
-
-  // return function(d, i) {
-
-  //   const linkLocked = self.state.link.id === d.id ? !self.state.linkLocked : self.state.linkLocked
-
-  //   self.setState({
-
-  //     link: d,
-
-  //     linkLocked: linkLocked
-
-  //   })
-
-  // }
-
-  // }
+   function mouseOverLink(d) {
+       console.log(d)
+       d3.select(this)
+         .style("stroke", "#F5352B");
+   }
 
 
-
-  // function mouseOverNode(d) {
-
-  // var self = this;
-
-  // return function(d, i) {
-
-  //   if(!self.state.nodeLocked){
-
-  //     self.setState({node: d})
-
-  //   }
-
-  // }
-
-  // }
-
-
-
-  // function mouseOverLink(d) {
-
-  //   var self = this;
-
-  //   return function(d, i) {
-
-
-
-  //     d3.select(this)
-
-  //       .style("stroke", "#F5352B");
-
-
-
-  //     self.setState({link: d})
-
-  //   }
-
-  // }
-
-
-
-  // function mouseOutLink(d) {
-
-  // d3.select(this).style("stroke", "#999");
-
-  // }
-
-
+   function mouseOutLink(d) {
+   d3.select(this).style("stroke", "#999");
+   }
 
 };
 
