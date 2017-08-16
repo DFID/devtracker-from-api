9c3a682611d4aedebf6274864bb5be0709e99458
Auninda <a-saleque@dfid.gov.uk>
2017-08-16 15:15:13 +0600

    removed a comment.

--- devtracker.rb
+++ devtracker.rb
@@ -55,7 +55,6 @@ include OGDHelper
 #set :oipa_api_url, 'https://devtracker.dfid.gov.uk/api/'
 #set :oipa_api_url, 'http://loadbalancer1-dfid.oipa.nl/api/'
 #set :oipa_api_url, 'https://staging-dfid.oipa.nl/api/'
-#set :oipa_api_url, 'http://localhost:8001/api/'
 #set :oipa_api_url, 'https://dev-dfid.oipa.nl/api/'
 
 # Server Machine: set global settings to use varnish cache
