aa823ccfb7ef2c4e6d22eb29e75f5833b728836e
Auninda <a-saleque@dfid.gov.uk>
2017-09-13 10:15:19 +0600

    .

---
+++ public/javascripts/indexTabs.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+    var chart = dc.rowChart("#test2");
+    d3.csv("/javascripts/morley.csv", function(error, experiments) {
+      experiments.forEach(function(x) {
+        x.Speed = +x.Speed;
+      });
+      var ndx                 = crossfilter(experiments),
+          runDimension        = ndx.dimension(function(d) {return +d.Run;}),
+          speedSumGroup       = runDimension.group().reduceSum(function(d) {return d.Speed * d.Run / 1000;});
+      chart
+        .width(350)
+        .height(150)
+        .x(d3.scale.linear().domain([6,20]))
+        .elasticX(true)
+        .dimension(runDimension)
+        .group(speedSumGroup)
+        .render();
+    });
+    <%
+    tempCountries = JSON.parse(top_5_countries.to_json)
+    tempCountries.each do |j|
+        j.delete("code")
+    end
+    %>
+    var top5Countries = <%=top_5_countries.to_json%>;
+    var top5Sectors = <%=what_we_do.to_json%>;
+    var chart2 = dc.rowChart("#top5Countries");
+
+
+    var info = crossfilter(top5Countries);
+    var dimentions = info.dimension(function(d){
+        return d.name;
+    });
+    var grouping = dimentions.group().reduceSum(function(d) {return d.budget;});
+    chart2
+    .width(320)
+    .height(420)
+    .x(d3.scale.ordinal())
+    .elasticX(true)
+    .dimension(dimentions)
+    .group(grouping)
+    .title(function(d){return "Budget: £" + d3.format(".4s")(d.value)})
+    .ordering(function(t){return t.budget;})
+    .cap(10)
+    .ordinalColors(['#D8DCBF'])
+    .xAxis().tickFormat(function(d){return "£" + d3.format(".2s")(d)}).ticks(4);
+
+    chart2.on('renderlet', function(chart) {
+        chart.selectAll("text").on("click", function (d) {
+            var result = $.grep(top5Countries,function(e){return e.name == d.key});
+            window.location.href = "countries/"+ result[0].code  + "/";
+        });
+    });
+
+    chart2.render();
+
+    var chart3 = dc.rowChart("#top5Sectors");
+    var info2 = crossfilter(top5Sectors);
+    var dimentions2 = info2.dimension(function(d){
+        return d.name;
+    });
+    var grouping2 = dimentions2.group().reduceSum(function(d) {return d.budget;});
+    chart3
+    .width(340)
+    .height(420)
+    .x(d3.scale.ordinal())
+    .elasticX(true)
+    .dimension(dimentions2)
+    .group(grouping2)
+    .title(function(d){return "Budget: £" + d3.format(".4s")(d.value)})
+    .ordering(function(t){return t.budget;})
+    .cap(10)
+    .ordinalColors(['#D8DCBF'])
+    .xAxis().tickFormat(function(d){return "£" + d3.format(".2s")(d)}).ticks(4);
+
+    chart3.on('renderlet', function(chart) {
+        chart.selectAll("text").on("click", function (d) {
+            var result = $.grep(top5Sectors,function(e){return e.name == d.key});
+            window.location.href = "sector/"+ result[0].code  + "/";
+        });
+    });
+
+    chart3.render();
+});
