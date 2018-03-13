34061f1115aaf37810abde465eb0d668e7946971
Auninda <a-saleque@dfid.gov.uk>
2018-03-13 13:12:09 +0600

    .

--- helpers/search_helper.rb
+++ helpers/search_helper.rb
@@ -125,35 +125,43 @@ Returns a Hash 'searchedData' with the following keys:
 		searchedData['actualStartDate'] = '1990-01-01T00:00:00'
 		searchedData['plannedEndDate'] = '2100-01-01T00:00:00'
 		# Pulling json data with an order by on actual start date to get the starting bound for the LHS date range slider. 
-		searchedData['actualStartDate'] = RestClient.get settings.oipa_api_url + "activities/?format=json&page_size=1&fields=activity_dates&hierarchy=1&q=#{query}&ordering=actual_start_date&start_date_gte=1900-01-02&activity_status="+activityStatusList
-		searchedData['actualStartDate'] = JSON.parse(searchedData['actualStartDate'])
-		if(searchedData['actualStartDate']['count'] > 0)
-			tempStartDate = searchedData['actualStartDate']['results'][0]['activity_dates'].select{|activityDate| activityDate['type']['code'] == '2'}.first
-			if (tempStartDate.nil?)
-				tempStartDate = searchedData['actualStartDate']['results'][0]['activity_dates'].select{|activityDate| activityDate['type']['code'] == '1'}.first
-			end
-	    	searchedData['actualStartDate'] = tempStartDate
-	    	searchedData['actualStartDate'] = searchedData['actualStartDate']['iso_date']
-	    else
-	    	searchedData['actualStartDate'] = '1990-01-01T00:00:00'
-	    end
+		begin
+			searchedData['actualStartDate'] = RestClient.get settings.oipa_api_url + "activities/?format=json&page_size=1&fields=activity_dates&hierarchy=1&q=#{query}&ordering=actual_start_date&start_date_gte=1900-01-02&activity_status="+activityStatusList
+			searchedData['actualStartDate'] = JSON.parse(searchedData['actualStartDate'])
+			if(searchedData['actualStartDate']['count'] > 0)
+				tempStartDate = searchedData['actualStartDate']['results'][0]['activity_dates'].select{|activityDate| activityDate['type']['code'] == '2'}.first
+				if (tempStartDate.nil?)
+					tempStartDate = searchedData['actualStartDate']['results'][0]['activity_dates'].select{|activityDate| activityDate['type']['code'] == '1'}.first
+				end
+		    	searchedData['actualStartDate'] = tempStartDate
+		    	searchedData['actualStartDate'] = searchedData['actualStartDate']['iso_date']
+		    else
+		    	searchedData['actualStartDate'] = '1990-01-01T00:00:00'
+		    end
+		rescue
+			searchedData['actualStartDate'] = '1990-01-01T00:00:00'
+		end
 		#unless searchedData['actualStartDate']['results'][0].nil? 
 		#	searchedData['actualStartDate'] = searchedData['actualStartDate']['results'][0]['activity_dates'][1]['iso_date']
 		#end
 		# Pulling json data with an order by on planned end date (DSC) to get the ending bound for the LHS date range slider. 
-		searchedData['plannedEndDate'] = RestClient.get settings.oipa_api_url + "activities/?format=json&page_size=1&fields=activity_dates&hierarchy=1&q=#{query}&ordering=-planned_end_date&end_date_isnull=False&activity_status="+activityStatusList
-		searchedData['plannedEndDate'] = JSON.parse(searchedData['plannedEndDate'])
-		#puts "activities/?format=json&page_size=1&fields=activity_dates&hierarchy=1&q=#{query}&ordering=-planned_end_date&end_date_isnull=False"
-		if(searchedData['plannedEndDate']['count'] > 0)
-			tempEndDate = searchedData['plannedEndDate']['results'][0]['activity_dates'].select{|activityDate| activityDate['type']['code'] == '3'}.first
-			if (tempEndDate.nil?)
-				tempEndDate = searchedData['plannedEndDate']['results'][0]['activity_dates'].select{|activityDate| activityDate['type']['code'] == '4'}.first
-			end
-	    	searchedData['plannedEndDate'] = tempEndDate
-	    	searchedData['plannedEndDate'] = searchedData['plannedEndDate']['iso_date']
-	    else
-	    	searchedData['plannedEndDate'] = '2100-01-01T00:00:00'
-	    end
+		begin
+			searchedData['plannedEndDate'] = RestClient.get settings.oipa_api_url + "activities/?format=json&page_size=1&fields=activity_dates&hierarchy=1&q=#{query}&ordering=-planned_end_date&end_date_isnull=False&activity_status="+activityStatusList
+			searchedData['plannedEndDate'] = JSON.parse(searchedData['plannedEndDate'])
+			#puts "activities/?format=json&page_size=1&fields=activity_dates&hierarchy=1&q=#{query}&ordering=-planned_end_date&end_date_isnull=False"
+			if(searchedData['plannedEndDate']['count'] > 0)
+				tempEndDate = searchedData['plannedEndDate']['results'][0]['activity_dates'].select{|activityDate| activityDate['type']['code'] == '3'}.first
+				if (tempEndDate.nil?)
+					tempEndDate = searchedData['plannedEndDate']['results'][0]['activity_dates'].select{|activityDate| activityDate['type']['code'] == '4'}.first
+				end
+		    	searchedData['plannedEndDate'] = tempEndDate
+		    	searchedData['plannedEndDate'] = searchedData['plannedEndDate']['iso_date']
+		    else
+		    	searchedData['plannedEndDate'] = '2100-01-01T00:00:00'
+		    end
+		rescue
+			searchedData['plannedEndDate'] = '2100-01-01T00:00:00'
+		end
 		#unless searchedData['plannedEndDate']['results'][0].nil?
 		#	if !searchedData['plannedEndDate']['results'][0]['activity_dates'][2].nil?
 		#		searchedData['plannedEndDate'] = searchedData['plannedEndDate']['results'][0]['activity_dates'][2]['iso_date']
