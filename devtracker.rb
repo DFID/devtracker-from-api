b048c4da8e410e79164315e6d94c3a16a0bfaab2
Auninda <a-saleque@dfid.gov.uk>
2018-06-25 10:43:50 +0600

    .

--- devtracker.rb
+++ devtracker.rb
@@ -56,9 +56,10 @@ include RecaptchaHelper
 #set :oipa_api_url, 'http://loadbalancer1-dfid.oipa.nl/api/'
 #set :oipa_api_url, 'https://staging-dfid.oipa.nl/api/'
 #set :oipa_api_url, 'https://dev-dfid.oipa.nl/api/'
+set  :oipa_api_url,	'https://integration.dfid.clouddev.works/api/'
 
 # Server Machine: set global settings to use varnish cache
-set :oipa_api_url, 'http://127.0.0.1:6081/api/'
+#set :oipa_api_url, 'http://127.0.0.1:6081/api/'
 
 #ensures that we can use the extension html.erb rather than just .erb
 Tilt.register Tilt::ERBTemplate, 'html.erb'
