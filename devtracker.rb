8f96a863e3ae7670ef6e9f61eae41246ab7a5210
Auninda <a-saleque@dfid.gov.uk>
2018-08-02 12:58:25 +0600

    .

--- devtracker.rb
+++ devtracker.rb
@@ -54,8 +54,9 @@ include RecaptchaHelper
 # Developer Machine: set global settings
 #set :oipa_api_url, 'https://devtracker.dfid.gov.uk/api/'
 #set :oipa_api_url, 'http://loadbalancer1-dfid.oipa.nl/api/'
-set :oipa_api_url, 'https://staging-dfid.oipa.nl/api/'
+#set :oipa_api_url, 'https://staging-dfid.oipa.nl/api/'
 #set :oipa_api_url, 'https://dev-dfid.oipa.nl/api/'
+set :oipa_api_url, 'https://dfid.oipa.nl/api/'
 
 # Server Machine: set global settings to use varnish cache
 #set :oipa_api_url, 'http://127.0.0.1:6081/api/'
