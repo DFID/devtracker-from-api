57766f679d8e66173e847000ca76a2375aac8f3f
Rumy <a-saleque@dfid.gov.uk>
2020-02-03 13:21:41 +0600

    Updated API URLs.

--- devtracker.rb
+++ devtracker.rb
@@ -53,10 +53,7 @@ include RecaptchaHelper
 
 # Developer Machine: set global settings
 #set :oipa_api_url, 'https://devtracker.dfid.gov.uk/api/'
-#set :oipa_api_url, 'http://loadbalancer1-dfid.oipa.nl/api/'
-#set :oipa_api_url, 'https://staging-dfid.oipa.nl/api/'
-#set :oipa_api_url, 'https://dev-dfid.oipa.nl/api/'
-#set :oipa_api_url, 'https://devtracker-entry.oipa.nl/api/'
+#set :oipa_api_url, 'https://devtracker-staging.oipa.nl/api/'
 
 # Server Machine: set global settings to use varnish cache
 set :oipa_api_url, 'http://127.0.0.1:6081/api/'
@@ -77,8 +74,8 @@ set :goverment_department_ids, 'GB-GOV-1,GB-1'
 set :google_recaptcha_publicKey, ENV["GOOGLE_PUBLIC_KEY"]
 set :google_recaptcha_privateKey, ENV["GOOGLE_PRIVATE_KEY"]
 
-set :raise_errors, true
-set :show_exceptions, true
+set :raise_errors, false
+set :show_exceptions, false
 
 set :devtracker_page_title, ''
 #####################################################################
