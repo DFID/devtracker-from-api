23a8a37e393b4bb5b2df4c9dbd1e876af1e51424
Rumy <a-saleque@dfid.gov.uk>
2021-05-10 12:21:23 +0600

    .

--- devtracker.rb
+++ devtracker.rb
@@ -55,13 +55,13 @@ include RecaptchaHelper
 include SolrHelper
 
 # Developer Machine: set global settings
-set :oipa_api_url, 'https://devtracker.fcdo.gov.uk/api/'
+#set :oipa_api_url, 'https://devtracker.fcdo.gov.uk/api/'
 #set :oipa_api_url, 'https://devtracker-staging.oipa.nl/api/'
 #set :oipa_api_url, 'https://iatidatastore.iatistandard.org/api/'
 #set :bind, '0.0.0.0' # Allows for vagrant pass-through whilst debugging
 
 # Server Machine: set global settings to use varnish cache
-#set :oipa_api_url, 'http://127.0.0.1:6081/api/'
+set :oipa_api_url, 'http://127.0.0.1:6081/api/'
 
 #set :oipa_api_url, 'https://iatidatastore.iatistandard.org/api/'
 
