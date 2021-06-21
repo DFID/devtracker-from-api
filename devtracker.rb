1bbde828c12080a45fb1805a40b074403884fff6
Rumy <a-saleque@dfid.gov.uk>
2021-06-21 15:04:38 +0600

    AB#13290 Removed sanitisaion on the project identifiers.

--- devtracker.rb
+++ devtracker.rb
@@ -339,7 +339,7 @@ end
 
 # Project summary page
 get '/projects/:proj_id/?' do |n|
-	n = sanitize_input(n,"p")
+	#n = sanitize_input(n,"p")
 	check_if_project_exists(n)
 	# get the project data from the API
   	project = get_h1_project_details(n)
@@ -391,7 +391,7 @@ end
 
 # Project documents page
 get '/projects/:proj_id/documents/?' do |n|
-	n = sanitize_input(n,"p")
+	#n = sanitize_input(n,"p")
 	# get the project data from the API
 	project = get_h1_project_details(n)
 
@@ -418,7 +418,7 @@ end
 
 #Project transactions page
 get '/projects/:proj_id/transactions/?' do |n|
-	n = sanitize_input(n,"p")
+	#n = sanitize_input(n,"p")
 	# get the project data from the API
 	project = get_h1_project_details(n)
 	componentData = get_project_component_data(project)
@@ -480,7 +480,7 @@ end
 #Project partners page
 get '/projects/:proj_id/partners/?' do |n|
 	# get the project data from the API
-	n = sanitize_input(n,"p")
+	#n = sanitize_input(n,"p")
 	project = get_h1_project_details(n)
 
   	#get the country/region data from the API
