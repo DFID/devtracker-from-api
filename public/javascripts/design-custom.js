9ecec49399d74b24b956dc0b2ce8c198d7c76b6a
Design <design@Opens-MacBook-Air.local>
2021-07-26 10:52:00 +0100

    Restyle of devtracker complete

---
+++ public/javascripts/design-custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+$(".show-advance-button").click(function(e){
+  $(".advance-toggle").addClass("active");
+  e.preventDefault();
+});
+
+
+$(".close-advance-button").click(function(e){
+  $(".advance-toggle").removeClass("active");
+  e.preventDefault();
+});
+
+$(document).click(function(event) {
+	event.preventDefault();
+  //if you click on anything except the modal itself or the "open modal" link, close the modal
+  if (!$(event.target).closest(".advance-toggle .inner,.show-advance-button").length) {
+    $("body").find(".advance-toggle").removeClass("active");
+  }
+});
+});
