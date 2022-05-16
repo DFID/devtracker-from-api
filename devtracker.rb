a265e0cdd365332e8a4991a1d1d9adc61a4677e2
Ubuntu <rumy_67@DevTracker-dev-vm.mptxugpdkkjefmcxs2gwssiuoe.zx.internal.cloudapp.net>
2022-05-16 10:10:09 +0000

    Setup ip for working from outside.

--- devtracker.rb
+++ devtracker.rb
@@ -62,7 +62,7 @@ include SolrHelper
 set :oipa_api_url, 'https://fcdo.iati.cloud/api/'
 set :oipa_api_url_solr, 'https://fcdo.iati.cloud/search/'
 # set :oipa_api_url, 'https://devtracker-staging.oipa.nl/api/'
-# set :bind, '0.0.0.0' # Allows for vagrant pass-through whilst debugging
+ set :bind, '0.0.0.0' # Allows for vagrant pass-through whilst debugging
 
 # Server Machine: set global settings to use varnish cache
 #set :oipa_api_url, 'http://127.0.0.1:6081/api/'
